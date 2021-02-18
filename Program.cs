422b70f899098138879b55a5080e68692beb6f01
Anton Pashkun <a.pashkun@godeltech.com>
2021-02-18 20:25:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyStackGenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack <string> myStack = new Stack <string> ();
+            myStack.Push("one");
+            myStack.Push("two");
+            myStack.Push("three");
+
+            string item1 = myStack.Pop();
+            string item2 = myStack.Peek();
+
+            myStack.Clear();
+
+            Console.WriteLine(item1);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyStackGenericsExample
+{
+    class Stack<T>
+    {
+        private int size;        // текущий размер стека
+        private List<T> data;    // данные стека
+
+        // конструктор
+        public Stack()
+        {
+            data = new List<T>();
+            size = 0;
+        }
+
+        // Метод добавление нового элемента
+        public void Push(T item)
+        {
+            data.Add(item);
+            size++;
+        }
+
+        // Метод извлечение элемента
+        public T Pop()
+        {
+            T item = data.LastOrDefault();
+            data.Remove(item);
+            size--;
+            return item;
+        }
+
+        // чтение главного элемента стека без удаления
+        public T Peek()
+        {
+            // если данных нет и Стек пуст то выбрасываем исключение
+            if (size == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return data [size -1];
+
+        }
+
+        // Метод очистки данных Стека
+        public void Clear() 
+        {
+            data.Clear();
+        }
+
+    }
+}
